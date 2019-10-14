7bcb14a4605a54b54de02967a3c97115926983a3
huchengpeng <2233749193@qq.com>
2019-10-14 16:02:37 +0800

    完成了CORS跨域访问配置文件

---
+++ manage-backend/src/main/java/edu/xpu/hcp/manage/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package edu.xpu.hcp.manage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**                                                                                ____________________
+      _                _                                                           < 神兽护体，永无bug! >
+    | |__  _   _  ___| |__   ___ _ __   __ _ _ __   ___ _ __   __ _                --------------------
+   | '_ \| | | |/ __| '_ \ / _ \ '_ \ / _` | '_ \ / _ \ '_ \ / _` |                       \   ^__^
+  | | | | |_| | (__| | | |  __/ | | | (_| | |_) |  __/ | | | (_| |                        \  (oo)\_______
+ |_| |_|\__,_|\___|_| |_|\___|_| |_|\__, | .__/ \___|_| |_|\__, |                           (__)\       )\/\
+                                   |___/|_|                |___/                                ||----w |
+                                                                                                ||     ||
+ * @author hcp
+ * @version 1.0.0
+ * @create 15:59 2019/10/14
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")//允许跨域访问的路径
+        .allowedOrigins("*")//允许跨域访问的源
+        .allowedMethods("POST","GET","PUT","OPTIONS","DELETE")//允许请求方法
+        .maxAge(168000)         //预检间隔时间
+        .allowedHeaders("*")    //允许头部设置
+        .allowCredentials(true);//是否发送cookie        
+        
+    }
+}
