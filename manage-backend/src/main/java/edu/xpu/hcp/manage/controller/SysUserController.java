a2e992053f65e803be73b7448ab13e3a332c1b06
huchengpeng <2233749193@qq.com>
2019-10-14 16:02:12 +0800

    完成Druid的设置

--- manage-backend/src/main/java/edu/xpu/hcp/manage/controller/SysUserController.java
+++ manage-backend/src/main/java/edu/xpu/hcp/manage/controller/SysUserController.java
@@ -2,9 +2,11 @@ package edu.xpu.hcp.manage.controller;
 import edu.xpu.hcp.manage.service.ISysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/sysuser/")
+@RestController
+@RequestMapping("/sysuser/")
 public class SysUserController {
 
     @Autowired
